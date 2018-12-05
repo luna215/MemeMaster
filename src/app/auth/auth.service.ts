576f4ba0569a6a07306ce9d750ba8457a7789510
Paul Luna <paulluna0215@gmail.com>
2018-12-05 15:41:34 -0800

    user now gets redirected when logging in or signing up.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 
@@ -6,20 +7,33 @@ import { AngularFireAuth } from 'angularfire2/auth';
 
 export class AuthService {
 
-  constructor(private firebase: AngularFireAuth) { }
+  successful: Promise<boolean>;
+  constructor(private firebase: AngularFireAuth, private router: Router) { }
 
 
   createUser(email, password) {
-    this.firebase.auth
+    this.successful = this.firebase.auth
         .createUserWithEmailAndPassword(email, password)
-        .then(function() { console.log('Successfull!'); })
-        .catch(function() { console.log('There was an error'); });
+        .then(function() { return true})
+        .catch(function() { return false });
+    if(!this.successful['i']){
+      alert('Uh oh! There was an issue. Try again.')
+      return;
+    }
+    this.router.navigate(['']);
   }
 
   login(email, password) {
-    this.firebase.auth
+    this.successful = this.firebase.auth
         .signInWithEmailAndPassword(email, password)
-        .then(function() { console.log('redirecting you now...'); })
-        .catch(function() { console.log(`Couldn't find a profile with those credentials`); });
+        .then(function() { return true; })
+        .catch(function() { return false; });
+    
+    if(!this.successful['i']) {
+      alert('Uh oh! We can\'t find an account with those credentials.' );
+      return;
+    }
+    
+    this.router.navigate(['']);
   }
 }
